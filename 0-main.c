c4d691bd29457eccc95583ac2fbc22ceae8ecdd6
kimaita <johnnieqaym@gmail.com>
2023-04-24 21:49:46 +0300

    added tests

--- 0-main.c
+++ 0-main.c
@@ -9,13 +9,33 @@
  */
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
+	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+	int array_wneg[] = {-46, -55, 92, 31, 150, 176, 137, -76, 105, 18};
+	int array_sorted[] = {-25, 41, 48, 71, 73, 94, 125, 143, 165, 181};
+	size_t n = sizeof(array) / sizeof(array[0]);
+	size_t n_wneg = sizeof(array_wneg) / sizeof(array_wneg[0]);
+	size_t n_sorted = sizeof(array_sorted) / sizeof(array_sorted[0]);
 
-    print_array(array, n);
-    printf("\n");
-    bubble_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
+	print_array(array, n);
+	printf("\n");
+	bubble_sort(array, n);
+	printf("\n");
+	print_array(array, n);
+
+	printf("\n-----\n");
+
+	print_array(array_wneg, n_wneg);
+	printf("\n");
+	bubble_sort(array_wneg, n_wneg);
+	printf("\n");
+	print_array(array_wneg, n_wneg);
+
+	printf("\n-----\n");
+
+	print_array(array_sorted, n_sorted);
+	printf("\n");
+	bubble_sort(array_sorted, n_sorted);
+	printf("\n");
+	print_array(array_sorted, n_sorted);
+	return (0);
 }
